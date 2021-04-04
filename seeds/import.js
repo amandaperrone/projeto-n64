b37aa4c524fe4cd6e62877c2e7c4742946cb6acf
Amanda Perrone <aperronee@gmail.com>
2021-04-04 13:01:37 -0300

    dia 01

--- seeds/import.js
+++ seeds/import.js
@@ -1,13 +1,67 @@
 const fs = require('fs');
 // npm install --save dotenv
-const dotenv = require('dotevn'); // Arquivos sensíveis 
+const dotenv = require('dotenv'); // Arquivos sensíveis 
 // npm install --save mongoose
 const  { Schema, model, connect} = require('mongoose');
 
 dotenv.config();
 
-console.log(process.env.DATABASE);
+const GameSchema = new Schema({title: String}, {strict: false})
+const Game = model('Game', GameSchema)
+
+const parseJSON = (data) => {
+    try {
+        return JSON.parse(data)
+    } catch (error) {
+        return null;
+    }
+}
+
+const connectToDB = () => {
+    const options = {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    };
+    return connect(process.env.DATABASE, options);
+}
 
 // Ler o arquivo .json
+const readGamesFromFile = (filename) => {
+    const promiseCallBack = (resolve, reject) => {
+
+        fs.readFile(filename, (err, data) => {
+            if(err) return reject(err);
+            const json = parseJSON(data)
+            if (!json) return reject(`Not able to parse JSON file ${filename}`)
+            return resolve(json);
+        });
+
+    };
+    
+    return new Promise(promiseCallBack);
+}
+
 // Fazer um loop entre os itens
-// Salvar cada um dos itens no banco
+const storeGame = (data) => { // salva o jogo no banco
+    const game = new Game(data)
+    return game.save()
+}
+
+const importGames = async () => {
+    await connectToDB() //conecta o banco
+
+    const games = await readGamesFromFile('games.json') // lê os jogos do arq
+
+    for(let i = 0; i < games.length; i++) { // salva os itens no banco
+        const game = games[i]
+        await storeGame(game)
+        console.log(game.title)
+    }
+
+    process.exit()
+}
+
+importGames()
+
